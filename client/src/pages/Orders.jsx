d95da9efae4fd429cca2536e0bc5197cae3ab43d
irshad ansari <ansariirshad1716@gmail.com>
2026-01-12 00:34:24 +0530

    placeorder and order page completed

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
 
 const Orders = () => {
+  const{products, currency} = useContext(ShopContext)
   return (
-    <div>Orders</div>
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title Text1={'MY'} Text2={'ORDERS'}/>
+      </div>
+      <div>
+        {products.slice(1, 4).map((item, index)=>(
+          <div key={index} className='py-4 border-t'>
+
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
